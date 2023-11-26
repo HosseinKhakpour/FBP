314a4612cefb8db3700bd3d05212504cbb3c62ac
h.khakpour <h.khakpour@LPNJR-CTM-RDS>
2023-11-26 11:05:05 +0330

    fix bug

--- src/Services/Identity/Fara.Services.Identity.Domain/Base/BaseResult_VM.cs
+++ src/Services/Identity/Fara.Services.Identity.Domain/Base/BaseResult_VM.cs
@@ -2,7 +2,7 @@
 
 public class BaseResult_VM<T>
 {
-    public T Result { get; set; }
+    public T? Result { get; set; } 
     public int Code { get; set; }
-    public string Message { get; set; }
+    public string Message { get; set; } = string.Empty;
 }
